40439027327d2b51231aa42ff787fcffe9c07977
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-06-24 21:18:11 -0400

    Created html elements for search input field and search button

--- src/create-html.js
+++ src/create-html.js
@@ -1,8 +1,54 @@
 export default class CreateHtml {
   static initializeHtml() {
+    this.createPageElements();
+    this.createSearchForm();
     this.createImageFrame();
     this.createImage();
     this.createGifButton();
+  }
+
+  static createPageElements() {
+    const header = document.createElement("header");
+    const main = document.createElement("main");
+    const footer = document.createElement("footer");
+
+    header.classList.add("header");
+    main.classList.add("main");
+    footer.classList.add("footer");
+
+    header.setAttribute("id", "header");
+    main.setAttribute("id", "main");
+    footer.setAttribute("id", "footer");
+
+    const body = document.querySelector("body");
+    body.appendChild(header);
+    body.appendChild(main);
+    body.appendChild(footer);
+  }
+
+  static createSearchForm() {
+    const searchForm = document.createElement("div");
+    searchForm.setAttribute("id", "search-form");
+    searchForm.setAttribute("action", "#");
+    searchForm.setAttribute("method", "post");
+
+    const input = document.createElement("input");
+    input.classList.add("search");
+    input.setAttribute("id", "search");
+    input.setAttribute("type", "text");
+    input.setAttribute("placeholder", "Search..");
+    input.setAttribute("name", "search");
+
+    const button = document.createElement("button");
+    button.textContent = "Search";
+    button.classList.add("button", "submit");
+    button.setAttribute("id", "submit-button");
+
+    searchForm.appendChild(input);
+    searchForm.appendChild(button);
+
+    const header = document.getElementById("header");
+    header.appendChild(searchForm);
   }
 
   static createImageFrame() {
@@ -10,8 +56,8 @@ export default class CreateHtml {
     imageFrame.classList.add("image-frame");
     imageFrame.setAttribute("id", "image-frame");
 
-    const body = document.querySelector("body");
-    body.appendChild(imageFrame);
+    const main = document.getElementById("main");
+    main.appendChild(imageFrame);
   }
 
   static createImage() {
@@ -27,7 +73,7 @@ export default class CreateHtml {
   static createGifButton() {
     const button = document.createElement("button");
     button.textContent = "New GIF";
-    button.classList.add("gif-button");
+    button.classList.add("button", "gif-button");
     button.setAttribute("id", "gif-button");
 
     const imageFrame = document.getElementById("image-frame");
